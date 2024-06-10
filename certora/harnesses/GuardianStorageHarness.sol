c60830bf4a77b52f3faff40e2e99b3d003d63fcc
Shebin John <admin@remedcu.com>
2024-06-10 16:42:44 +0200

    Changes to Guardian Storage Harness

--- certora/harnesses/GuardianStorageHarness.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.12 <0.9.0;
-
-import {GuardianStorage} from "../../contracts/modules/social_recovery/storage/GuardianStorage.sol";
-
-contract GuardianStorageHarness is GuardianStorage {}
