0e74a0cc80b75cebbbef0898e775a6f647514559
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-06-14 10:10:54 +0200

    add docs to the harnessed func

--- certora/harnesses/SocialRecoveryModuleHarness.sol
+++ certora/harnesses/SocialRecoveryModuleHarness.sol
@@ -31,7 +31,20 @@ contract SocialRecoveryModuleHarness is SocialRecoveryModule {
         }
     }
 
+    /**
+     * @notice Verifies a series of signatures associated with a wallet recovery process.
+     *         The function is copied from `multiConfirmRecovery` without the storage modifications.
+     * @dev This function checks the validity and order of signatures for a wallet recovery hash.
+     *      It ensures that all signatures are from the wallet's guardians and that they are in
+     *      ascending order to prevent duplicates. Null signatures must have the sender as the signer and the sender
+     *      must be a guardian.
+     * @param _wallet The address of the wallet being recovered.
+     * @param recoveryHash The hash of the recovery data which needs to be signed by the guardians.
+     * @param _signatures An array of SignatureData structures containing the signer's address and their signature.
+     */
     function checkSignatures(address _wallet, bytes32 recoveryHash, SignatureData[] memory _signatures) public view {
+        require(_signatures.length > 0, "SM: empty signatures");
+
         address lastSigner = address(0);
         for (uint256 i = 0; i < _signatures.length; i++) {
             SignatureData memory value = _signatures[i];
